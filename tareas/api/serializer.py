225ded8d9ecd419dd53b8d7835069c463d3b0225
Juan P Mendieta C <jpmendietac@ut.edu.co>
2023-03-23 15:43:16 -0500

    Validation deploy

--- tareas/api/serializer.py
+++ tareas/api/serializer.py
@@ -13,22 +13,25 @@ class UserSerializer(serializers.ModelSerializer):
     def validate_name(self, name):
         if name == '':
             raise serializers.ValidationError('El nombre no puede estar vacio')
-        if name.isalpha():
-            return name
-        
+        name = name.strip().title()  # Convierte la primera letra de cada palabra a mayúscula
+        if not all(word.isalpha() for word in name.split()):
+            raise serializers.ValidationError('El nombre solo puede contener letras')
+        return name
+
     def validate_last_name(self, last_name):
         if last_name == '':
             raise serializers.ValidationError(
                 'El apellido no puede estar vacio')
-        if last_name.isalpha():
-            return last_name
+        last_name = last_name.strip().title()  # Convierte la primera letra de cada palabra a mayúscula
+        if not all(word.isalpha() for word in last_name.split()):
+            raise serializers.ValidationError('El apellido solo puede contener letras')
+        return last_name
+
+
              
     def validate_email(self, value):
         if not value:
             raise serializers.ValidationError('Tiene que indicar un correo.')
-        elif self.initial_data.get('name') in value:
-            raise serializers.ValidationError(
-                'El email no puede contener el nombre')
         elif not re.match(r"[^@]+@[^@]+\.[^@]+", value):
             raise serializers.ValidationError(
                 'El correo electrónico no es válido')
@@ -76,17 +79,59 @@ class UserSerializer(serializers.ModelSerializer):
             existing_user.save()
             return existing_user
 
-        return super().create(validated_data)
+        name = validated_data.get('name', '').capitalize()
+        last_name = validated_data.get('last_name', '').capitalize()
+        user = User.objects.create(
+            name=name,
+            last_name=last_name,
+            email=email,
+            phone_number=phone_number,
+            active=True
+        )
+        return user
 
     def update(self, instance, validated_data):
         self.validate_active(instance.active)
         validated_data = self.validate(validated_data)
+
         if validated_data.get('name'):
-            instance.name = validated_data.get('name').capitalize()
-        if validated_data.get('active'):
-            instance.active = validated_data.get('active')
+            validated_data['name'] = ' '.join(word.capitalize() for word in validated_data['name'].split())
+
+        if validated_data.get('last_name'):
+            validated_data['last_name'] = ' '.join(word.capitalize() for word in validated_data['last_name'].split())
+
+        instance.__dict__.update(validated_data)
+        instance.save()
+
+        return instance
+
+
+    def partial_update(self, instance, validated_data):
+        # Actualización parcial del objeto
+        # Recorre los campos que se quieren actualizar y los actualiza
+        for field, value in validated_data.items():
+            if field == 'name':
+                instance.name = value.capitalize()
+            elif field == 'last_name':
+                instance.last_name = value.capitalize()
+            elif field == 'email':
+                instance.email = value
+            elif field == 'phone_number':
+                instance.phone_number = value
+            elif field == 'active':
+                self.validate_active(value)
+                instance.active = value
         instance.save()
         return instance
+    
+    def delete(self, instance):
+        if instance.active == False:
+            raise serializers.ValidationError(
+                'No existe este usuario en la base de datos')
+        else:
+            instance.active = False
+            instance.save()
+            return {'message': 'Usuario eliminado correctamente'}
 
 
 
@@ -132,6 +177,33 @@ class HomeworkSerializer(serializers.ModelSerializer):
 
         return super().create(validated_data)
     
+    
+    def update(self, instance, validated_data):
+        self.validate_active(instance.active)
+        validated_data = self.validate(validated_data)
+
+        if validated_data.get('title'):
+            validated_data['title'] = ' '.join(word.capitalize() for word in validated_data['title'].split())
+
+        instance.__dict__.update(validated_data)
+        instance.save()
+
+        return instance
+
+
+    def partial_update(self, instance, validated_data):
+        for field, value in validated_data.items():
+            if field == 'title':
+                instance.name = value.capitalize()
+            elif field == 'time':
+                instance.last_name = value.capitalize()
+            elif field == 'active':
+                instance.email = value
+            elif field == 'user':
+                instance.phone_number = value
+        instance.save()
+        return instance
+
     
     
     def delete(self, instance):
